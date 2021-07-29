0e822437b3c229f34d3a4536f44c9ccd83a98926
preetip26 <preeti.poddar@somaiya.edu>
2021-07-29 12:48:05 +0530

    NUnit testing Project

---
+++ UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+using ToBeTested;
+
+namespace NUnitPart2Jenkins
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        Program p = new Program();
+        [Test]
+        public void TestAdd()
+        {
+            int val = p.Add(5, 4);
+            Assert.AreEqual(10, val);
+        }
+    }
+}
